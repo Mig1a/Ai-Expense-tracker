10bda808bfb4d569c32520cbb98dfffbfc032a59
Million Aboye <111463664+Mig1a@users.noreply.github.com>
2026-02-16 14:21:43 -0500

    Update Supabase URL and anon key

--- Frontend/src/supabaseClient.js
+++ Frontend/src/supabaseClient.js
@@ -1,8 +1,8 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://bpxpulbsxuyjbfbtcnyd.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJweHB1bGJzeHV5amJmYnRjbnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4NDU0MTQsImV4cCI6MjA2MzQyMTQxNH0.cl60ugopz75vLalU4Z9GHJcw357wK_YryUk-2iVmT38'
+const supabaseUrl = 'https://hmncstlbxbpzoyeuahtm.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtbmNzdGxieGJwem95ZXVhaHRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNjQzNzIsImV4cCI6MjA4Njg0MDM3Mn0.eTyUusNsAGG91PriUCxkH0nMiEXJokmssfZSI1I6qnY'
 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
